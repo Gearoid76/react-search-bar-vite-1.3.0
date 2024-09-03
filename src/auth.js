02c7357adbfb48dfe616a18ee5302bae422aa584
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-09-03 14:19:27 +0300

    space usage blank space

--- src/auth.js
+++ src/auth.js
@@ -22,7 +22,6 @@ export async function redirectToAuthCodeFlow(clientId) {
     document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
 }
 
-
 export function generateCodeVerifier(length) {
     let text = '';
     let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
