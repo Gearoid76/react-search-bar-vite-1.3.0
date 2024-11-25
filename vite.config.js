689f590f5019b985abb3bbd1c80c67af185cf42d
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-11-25 14:38:24 +0200

    changed the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from "vite";
+
+/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,3 +9,15 @@ export default defineConfig({
   base:"/react-search-bar-vite-1.3.0/",
   plugins: [react()],
 })
+  */
+export default  defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "404.html"),
+      },
+    },
+  },
+});
