9c2831ce1c1f08821541691997551d874f412b33
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-11-25 15:09:49 +0200

    .github/workflows/deploy.yml for workflows

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,3 @@
-import { defineConfig } from "vite";
-
-/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -9,15 +6,3 @@ export default defineConfig({
   base:"/react-search-bar-vite-1.3.0/",
   plugins: [react()],
 })
-  */
-export default  defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "404.html"),
-      },
-    },
-  },
-});
