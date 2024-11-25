3f7eff8e406b54b8ed5c5d1d9da0b4ff9193dae2
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-11-25 15:21:55 +0200

    swapped the vite.config.js plugins and base around

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/react-search-bar-vite-1.3.0/",
   plugins: [react()],
+  base:"/react-search-bar-vite-1.3.0/",
+  
 })
