e041c7d6b666356451b222492e4702e32c39d84e
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-11-26 18:01:33 +0200

    updated App.js with Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,35 @@
-// src/App.jsx
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { redirectToAuthCodeFlow, getAccessToken } from './auth';
 import { SearchBar } from './components/SearchBar';
 import { SearchResultsList } from './components/SearchResultsList';
-//import Footer from './components/Footer';
+import CallbackPage from './components/CallbackPage';
 import './App.css';
 
 export function App() {
-  const [accessToken, setAccessToken] = useState(''); 
+  const [accessToken, setAccessToken] = useState('');
   const [results, setResults] = useState([]);
-  const [playlist, setPlaylist] = useState(null); 
+  const [playlist, setPlaylist] = useState(null);
   const [playlistTracks, setPlaylistTracks] = useState([]);
-
 
   useEffect(() => {
     const clientId = import.meta.env.VITE_CLIENT_ID;
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get("code");
-
-    if (!code) {
+    if (!accessToken) {
       redirectToAuthCodeFlow(clientId);
-    } else {
-      getAccessToken()
-        .then((token) => {
-          setAccessToken(token);
-          console.log('AccessToken Set:', token);
-        })
-        .catch((error) => console.error("Error fetching access token:", error));
     }
-  }, []);
+  }, [accessToken]);
 
   const handleAddToPlaylist = (tracks, playlistName) => {
     if (!accessToken) {
-      console.error("No access token available");
-      return Promise.reject(new Error("No access token available")); // Return rejected Promise
+      console.error('No access token available');
+      return Promise.reject(new Error('No access token available'));
     }
-  
+
     // Create a new playlist
     return fetch('https://api.spotify.com/v1/me/playlists', {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${accessToken}`,
+        Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
@@ -49,71 +38,105 @@ export function App() {
         public: false,
       }),
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         const playlistId = data.id;
         setPlaylist({ id: playlistId, name: playlistName });
-  
-        const uris = tracks.map(track => `spotify:track:${track.id}`);
-  
+
+        const uris = tracks.map((track) => `spotify:track:${track.id}`);
+
         // Add the tracks to the newly created playlist
         return fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
           method: 'POST',
           headers: {
-            'Authorization': `Bearer ${accessToken}`,
+            Authorization: `Bearer ${accessToken}`,
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
             uris: uris,
           }),
         })
-          .then(response => response.json())
+          .then((response) => response.json())
           .then(() => {
             setPlaylistTracks(tracks);
             return Promise.resolve();
           });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error creating or adding tracks to playlist:', error);
         return Promise.reject(error);
       });
   };
-  
-
 
   return (
-    <div className="App">
-      <div className="headder">
-        <h1>Muusic(k)</h1>
-      </div>
-      <div className="search-bar-container">
-        <SearchBar setResults={setResults} accessToken={accessToken} />
-        <SearchResultsList results={results} onAddToSpotify={handleAddToPlaylist} />
-      </div> 
-      {playlist && (
-        <div className="playlist-display">
-          <h2>Playlist: {playlist.name}</h2>
-          <div className="playlist-tracks">
-            {playlistTracks.length > 0 ? (
-              playlistTracks.map((track, index) => (
-                <div key={index} className="playlist-track">
-                  <img src={track.imageUrl} alt={track.name} style={{ width: '50px', height: '50px' }} />
-                  <div>
-                    <h3>{track.name}</h3>
-                    <p>{track.artist} - {track.album}</p>
-                    <a href={track.externalUrl} target="_blank" rel="noopener noreferrer">Listen on Spotify</a>
-                  </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Route for Callback Page */}
+          <Route
+            path="/callback"
+            element={
+              <CallbackPage
+                setAccessToken={setAccessToken}
+                getAccessToken={getAccessToken}
+              />
+            }
+          />
+          {/* Main App Route */}
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="headder">
+                  <h1>Muusic(k)</h1>
                 </div>
-              ))
-           ) : (
-              <p>No tracks added yet.</p>
-            )}
-          </div>
-        </div> 
-      )}
-     
-    </div>
-    
+                <div className="search-bar-container">
+                  <SearchBar setResults={setResults} accessToken={accessToken} />
+                  <SearchResultsList
+                    results={results}
+                    onAddToSpotify={handleAddToPlaylist}
+                  />
+                </div>
+                {playlist && (
+                  <div className="playlist-display">
+                    <h2>Playlist: {playlist.name}</h2>
+                    <div className="playlist-tracks">
+                      {playlistTracks.length > 0 ? (
+                        playlistTracks.map((track, index) => (
+                          <div key={index} className="playlist-track">
+                            <img
+                              src={track.imageUrl}
+                              alt={track.name}
+                              style={{ width: '50px', height: '50px' }}
+                            />
+                            <div>
+                              <h3>{track.name}</h3>
+                              <p>
+                                {track.artist} - {track.album}
+                              </p>
+                              <a
+                                href={track.externalUrl}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                Listen on Spotify
+                              </a>
+                            </div>
+                          </div>
+                        ))
+                      ) : (
+                        <p>No tracks added yet.</p>
+                      )}
+                    </div>
+                  </div>
+                )}
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
